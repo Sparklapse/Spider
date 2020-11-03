045089e26f5279cd970fcce16c92c10c75fcc251
mr-josh <joshua@sparklapse.com>
2020-11-03 20:54:45 +1100

    🔨 Cleaned and updated WebServer class
    - Removed app array
    - Added routes for 404 and 500
    - Added exception handling and returning 500
    - Removed start function

--- spider/web/_server.py
+++ spider/web/_server.py
@@ -1,44 +1,48 @@
-import re
 import importlib
+import logging
+import re
 
 from .._server import Server
 from ._http import HTTPRequest, HTTPResponse
-from ._service import WebService
 from ._route import Route
+from ._service import WebService
+
 
 class WebServer(Server):
     service = WebService
 
-    apps = []
-
     routes = []
-    end_route = HTTPResponse(
+    route_not_found = HTTPResponse(
         content="404",
         code=404,
-        status="Not Found"
+        status="Not Found",
+        headers={
+            "Content-Type": "text/plain"
+        }
     )
-
-    def start(self):
-        for app in self.apps:
-            a = importlib.import_module(app)
-            for r in list(filter(
-                        lambda i: isinstance(getattr(a, i), Route),
-                        dir(a)
-                    )):
-                route = getattr(a, r)
-                self.routes.append(route)
+    route_server_error = lambda s, e: HTTPResponse(
+        content=f"500\n{e}",
+        code=500,
+        status="Server Error",
+        headers={
+            "Content-Type": "text/plain"
+        }
+    )
 
     def receiver(self, request: HTTPRequest):        
         for route in self.routes:
-            if (re.fullmatch(route.path, request.path)
-                and re.fullmatch(route.domain, request.domain)
-                    ):
-                print(
-                    request.method,
-                    request.domain,
-                    request.path,
-                    request.params
-                )
-                return route(request)
+            try:
+                if (re.fullmatch(route.path, request.path)
+                        and re.fullmatch(route.domain, request.domain)):
+                    print(
+                        request.method,
+                        request.domain,
+                        request.path,
+                        request.params
+                    )
+
+                    return route(request)
+            except Exception as e:
+                return self.route_server_error(e)
         
-        return self.end_route
+        return self.route_not_found
