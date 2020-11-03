fd16cdc64ca2e9f0f697f9fc7cbfb9ed9e1e8eea
mr-josh <joshua@sparklapse.com>
2020-11-03 20:48:08 +1100

    🔨 Changed Route class to not work as a decorator

--- spider/web/_route.py
+++ spider/web/_route.py
@@ -1,17 +1,24 @@
+from ._http import HTTPRequest, HTTPResponse
+
+
 class Route():
-    def __init__(self, path, domain=r".*", renderer=None):
+    """ Route
+    A standard route that calls a HTTPResponse page
+    """
+    def __init__(self, path, domain=r".*", page=None):
         self.path = path
         self.domain = domain
-        self.renderer = renderer
+        self.page = page
+
+        if not callable(page):
+            raise TypeError("Page is not callable")
     
-    def __call__(self, *args, **kwargs):
-        if not self.renderer:
-            if not callable(args[0]):
-                raise TypeError(
-                    ("Must be a render function that returns a HTTPResponse.\n"
-                    f"Returned {args[0]}")
-                )
-            self.renderer = args[0]
-            return self
-        else:
-            return self.renderer(*args, **kwargs)
+    def __call__(self, request):
+        if not isinstance(request, HTTPRequest):
+            raise TypeError("Request is not HTTPRequest")
+
+        if not isinstance((response := self.page(request)), HTTPResponse):
+            raise TypeError("Page did not return a HTTPResponse")
+
+        return response
+
