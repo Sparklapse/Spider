5ba7c90211328bc260211f879d049aa97e134822
mr-josh <joshua@sparklapse.com>
2020-12-29 11:14:32 +1100

    🔨 Added RouteManager

--- spider/web/__init__.py
+++ spider/web/__init__.py
@@ -1,6 +1,6 @@
 from ._http import HTTPRequest, HTTPResponse
 from ._render import render
-from ._route import Route
+from ._route import Route, RouteManager
 from ._server import Server
 from ._service import WebService
 from ._static import StaticDeliverer
@@ -8,7 +8,7 @@ from ._static import StaticDeliverer
 __all__ = [
     'HTTPRequest', 'HTTPResponse',
     'render',
-    'Route',
+    'Route', 'RouteManager',
     'Server',
     'WebService',
     'StaticDeliverer',
--- spider/web/_route.py
+++ spider/web/_route.py
@@ -1,3 +1,4 @@
+import re
 from ._http import HTTPRequest, HTTPResponse
 
 
@@ -17,8 +18,46 @@ class Route():
         if not isinstance(request, HTTPRequest):
             raise TypeError("Request is not HTTPRequest")
 
-        if not isinstance((response := self.page(request)), HTTPResponse):
-            raise TypeError("Page did not return a HTTPResponse")
+        response = self.page(request)
+        if isinstance(response, str):
+            return HTTPResponse(response)
+        elif isinstance(response, HTTPResponse):
+            return response
+        else:
+            raise TypeError("Page did not return a valid response")
 
-        return response
+class RouteManager():
+    routes = []
+    not_found = HTTPResponse(
+        content="404",
+        code=404,
+        status="Not Found",
+        headers={
+            "Content-Type": "text/plain"
+        }
+    )
+    server_error = lambda s, e: HTTPResponse(
+        content=f"500\n{e}",
+        code=500,
+        status="Server Error",
+        headers={
+            "Content-Type": "text/plain"
+        }
+    )
 
+    def __init__(self, routes=None, not_found=None, server_error=None):
+        self.routes = self.routes or routes
+        self.not_found = self.not_found or not_found
+        self.server_error = self.server_error or server_error
+        
+
+    def __call__(self, request: HTTPRequest):
+        for route in self.routes:
+            try:
+                if (re.fullmatch(route.path, request.path)
+                        and re.fullmatch(route.domain, request.domain)):
+                    if (r := route(request)): return r
+            except Exception as e:
+                return self.server_error(e)
+        
+        return self.not_found
